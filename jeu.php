2ec16ec2181e0e2d07ce37425ef6799fa482e589
cedrickevers <keverscedric@gmail.com>
2019-11-04 01:34:20 +0100

    finish form conditions

--- jeu.php
+++ jeu.php
@@ -1,19 +1,29 @@
 <?php
-$aDeviner= 150;
-$test =$_GET["chiffre"];
+ $aDeviner = 150;
+ $erreur = null;
+ $succes = null;
+ $value;
+ if(isset($_GET["chiffre"])) {
+     if($_GET["chiffre"] > $deviner){
+         $erreurr = "trop Grand"; 
+     }
+     elseif($_GEt["chiffre"] < $aDeviner){
+         $erreur ="Votre chiffre est trop etit";
+     }
+     else{
+         $succes ="GG vous avez trouvez le bon nombre <strong>$aDeviner</strong>";
+     }
+     $value = (int)$_GEt("chiffre");
+ }
  ?>
-
-<?php if($test> $aDeviner):?>
- trop grand
-<?php elseif($test< $aDeviner):?>
-trop petit
-<?php else: ?>
-Bravo
-<?php endif ?>
+ <?php if($erreur):?>
+ <div class="alert alert-danger">
 
 <form action="/jeu.php" method="GET">
+<?php return $error;?>
 <input type="number" name="chiffre" placeholder = "entre 0 et 1000" value="<?php htmlentities($test) ?>">
 <button type ="submit">Deviner</button>
 </form>
 
-<?php require ("footer.Php");
+
+ 

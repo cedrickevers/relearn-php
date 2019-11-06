b29730b716e46ea9cd5fe68b320a089ebcc047ef
cedrickevers <keverscedric@gmail.com>
2019-11-06 11:14:28 +0100

    set a sample checkbox form

--- jeu.php
+++ jeu.php
@@ -1,42 +1,8 @@
 
 <?php
+
+?>
 //DECLARATION
-$aDeviner = 150;
-$erreur = null;
-$succes = null;
-?>
-<?php
-//Starting conditions
-
-if(isset($_GET["chiffre"])) {
-     if($_GET["chiffre"] > $aDeviner){
-         $erreur = "trop Grand"; 
-     }
-     elseif($_GET["chiffre"] < $aDeviner){
-         $erreur ="Votre chiffre est trop etit";
-     }
-     else{
-         $succes ="GG vous avez trouvez le bon nombre <strong>$aDeviner</strong>";
-     }
-     $value =(int) $GET_["chiffre"];
-
- }
-?>
-<?php 
-//conditions résults
-require "header.php";
-if($erreur):?>
- <div class="alert alert-danger">
-    <?= $erreur?>
- </div>
- <?php elseif ($succes): ?>
- <div class="alert alert-sucess">
-    <?= $succes?>
-     
- </div>
- <?php endif?>
-
-
 
  <form action="/jeu.php" method="GET">
     <div class="form-group">

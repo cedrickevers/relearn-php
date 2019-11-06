d7e08555d31c329ec8ab7d2c372cd619a0c40690
cedrickevers <keverscedric@gmail.com>
2019-11-06 16:20:22 +0100

    function that generate radio and checkbox

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+function checkbox (string $name, string $value, array $data): string{
+    $attributes =""; 
+    if( isset($data[$name]) &&  in_array($data_name)){
+        $attributes = "checked";
+    }
+    return <<<HTML
+        <input type="checkbox" name="{$name}[]" value="$value">
+HTML;        
+}
+
+
+function radio (string $name, string $value, array $data): string{
+    $attributes =""; 
+    if( isset($data[$name]) &&  value === $data[$name]){
+        $attributes = "checked";
+    }
+    return <<<HTML
+        <input type="radio" name="{$name}" value="$value">
+HTML;        
+}
+?>
--- jeu.php
+++ jeu.php
@@ -1,16 +1,74 @@
 
+
 <?php
 
-?>
-//DECLARATION
+require_once "header.php";
+require_once "functions.php";
 
- <form action="/jeu.php" method="GET">
+$parfums = ["vanille" => 3, "chocolat" => 4, "pomme"=> 5];
+$cornets = ["corner" => 1.5, "ravier" => 1];
+$suplements = ["Granulé "=>0.5,  "chantilly"=> 5];
+?>
+
+<div class="row">
+
+<div class="col-md-4">
+
+</div>
+<div class="col-md-8">
+
+<form action="/jeu.php" method="GET">
     <div class="form-group">
-    <input type="checkbox" name="parfum" value="Fraise"> Fraise <br/>
-    <input type="checkbox" name="parfum" value="Vanille"> Vanille <br/>
-    <input type="checkbox" name="parfum" value="Chocolat">chocolat <br/>
-    </div>
+   
+    <?php foreach($parfums as $parfum => $prix) : ?>
+            <div class="checkbox">
+                <label>
+                    <?= checkbox("parfum", $parfum, $_GET) ?>
+                    <?= $parfum ?> - <?= $prix ?> €
+                </label>
+            </div>
+            <?php endforeach ?>
+
+            <?php foreach($cornets as $cornet => $prix) : ?>
+                <div class="checkbox">
+                    <label>
+                        <?= radio("cornet", $cornet, $_GET)?>
+                        <?= $cornet ?> - <?= $prix ?> €
+                        
+                    </label>
+                </div>
+
+            <?php endforeach ?>
+
+            <?php foreach($suplements as $suplement => $prix) : ?>
+            <div class="checkbox">
+                <label>
+                    <?= checkbox("parfum", $suplement, $_GET) ?>
+                    <?= $suplement ?> - <?= $prix ?> €
+                </label>
+            </div>
+            <?php endforeach ?>
+
+
+    <button type="submit" class="btn">TEST</button>
 </form>
+     </div>
+</div>
+
+     <?php echo "Le prix est de $prize;"?>
+    
+<h2>$_POST</h2>
+<pre>
+    <?php var_dump($_POST) ?>
+</pre>
+
+
+<h2>$_get</h2>
+<pre>
+    <?php 
+    var_dump($_GET)
+    ?> 
+
 
 <style>
 .alert,form{
